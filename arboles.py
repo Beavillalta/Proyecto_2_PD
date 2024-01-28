7463221cb92ca0c7542602effce136d94f9df6cb
Beavillalta <146924222+Beavillalta@users.noreply.github.com>
2024-01-27 22:06:17 -0600

    Clasificacion 2

--- arboles.py
+++ arboles.py
@@ -1,4 +1,4 @@
-# Parte 11 del proyecto
+# Parte 12 del proyecto
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -6,23 +6,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
-def graficar_distribucion_clases_1(df, columna_clases='DEATH_EVENT'):
+def graficar_distribucion_clases_2(df):
     # Paso 1: Graficar la distribución de clases utilizando seaborn
     plt.figure(figsize=(6, 4))
-    sns.countplot(x=columna_clases, data=df)
+    sns.countplot(x='DEATH_EVENT', data=df)
     plt.title('Distribución de Clases')
-    plt.xlabel(columna_clases)
+    plt.xlabel('DEATH_EVENT')
     plt.ylabel('Cantidad de Observaciones')
-    plt.savefig('distribucion_clases.png')  # Guardar la figura como imagen
     plt.show()
 
-def particion_estratificada_1(df, columna_clases='DEATH_EVENT'):
+def particion_estratificada_2(df):
     # Paso 2: Realizar la partición estratificada del dataset
-    X = df.drop(columns=[columna_clases])
-    y = df[columna_clases]
+    X = df.drop(columns=['DEATH_EVENT'])
+    y = df['DEATH_EVENT']
     return train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-def ajustar_arbol_decision_1(X_train, X_test, y_train, y_test):
+def ajustar_arbol_decision_2(X_train, X_test, y_train, y_test):
     # Paso 3: Ajustar un árbol de decisión y calcular el accuracy
     # Paso 4: Experimentar con ajustes de parámetros para mejorar la precisión
     modelo_arbol = DecisionTreeClassifier(random_state=42)
@@ -31,19 +30,13 @@ def ajustar_arbol_decision_1(X_train, X_test, y_train, y_test):
     precision = accuracy_score(y_test, y_pred)
     print(f"Precisión del modelo de árbol de decisión: {precision}")
 
-    # Opcional: Imprimir la matriz de confusión
-    from sklearn.metrics import confusion_matrix
-    matriz_confusion = confusion_matrix(y_test, y_pred)
-    print("Matriz de Confusión:")
-    print(matriz_confusion)
-
 import pandas as pd
- 
+
 # Cargar el DataFrame con los datos procesados
 df = pd.read_csv('datos_procesados.csv')
 df = df.drop(columns=['Grupo'])
 
 # Utilizar las funciones de clasificación
-graficar_distribucion_clases_1(df)
-X_train, X_test, y_train, y_test = particion_estratificada_1(df)
-ajustar_arbol_decision_1(X_train, X_test, y_train, y_test)
+graficar_distribucion_clases_2(df)
+X_train, X_test, y_train, y_test = particion_estratificada_2(df)
+ajustar_arbol_decision_2(X_train, X_test, y_train, y_test)
